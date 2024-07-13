52ef42c1b2be8a3e441e103bba8cd3d61bef32b1
Tatiana Chepurnaya <tanyusha_tokor@mail.ru>
2024-07-13 14:38:26 +0500

    add code

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -1,5 +1,8 @@
+from django.urls import path
+
 from vehicle.apps import VehicleConfig
-from vehicle.views import CarViewSet
+from vehicle.views import CarViewSet, MotoCreateAPIView, MotoListAPIView, MotoRetrieveAPIView, MotoUpdateAPIView, \
+    MotoDestroyAPIView
 from rest_framework.routers import DefaultRouter
 
 app_name = VehicleConfig.name
@@ -9,5 +12,9 @@ router = DefaultRouter()
 router.register(r'cars', CarViewSet, basename='cars')
 
 urlpatterns = [
-
+    path('moto/create/', MotoCreateAPIView.as_view(), name='moto-create'),
+    path('moto/', MotoListAPIView.as_view(), name='moto-list'),
+    path('moto/<int:pk>/', MotoRetrieveAPIView.as_view(), name='moto-detail'),
+    path('moto/update/<int:pk>/', MotoUpdateAPIView.as_view(), name='moto-update'),
+    path('moto/delete/<int:pk>/', MotoDestroyAPIView.as_view(), name='moto-delet'),
 ] + router.urls
